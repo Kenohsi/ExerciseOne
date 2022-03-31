07b0a5eb6abce3b872b603956bedb8ce655d95e0
kenan <kenan.husic@stud.fh-campuswien.ac.at>
2022-03-31 23:11:55 +0200

    Updated FXML

--- src/main/java/at/ac/fhcampuswien/NewsAppFx.java
+++ src/main/java/at/ac/fhcampuswien/NewsAppFx.java
@@ -1,21 +1,12 @@
 package at.ac.fhcampuswien;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.image.ImageView;
-
-
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class NewsAppFx extends AppController {
 
@@ -72,37 +63,9 @@ public class NewsAppFx extends AppController {
          return null;
       }
    };
-   private List<Article> articles = new ArrayList<Article>();
 
-   @FXML
-   private Button Headlines = new Button();
 
-   @FXML
-   private Button Bitcoin = new Button();
 
-   @FXML
-   private Button ArticleCount = new Button();
-
-   @FXML
-   private Button Quit = new Button();
-
-   @FXML
-   private ImageView BreakingPicture;
-
-   @FXML
-   private ImageView TitlePicture;
-
-   @FXML
-   private ImageView NewsPicture;
-
-   @FXML
-   private ImageView ArticlesPics;
-
-   @FXML
-   private ImageView BitcoinPicture;
-
-   @FXML
-   private TextArea Text;
 
    public void BitcoinNews(ActionEvent actionEvent) {
      Labelonee.setText(getAllNewsBitcoin().toString());
