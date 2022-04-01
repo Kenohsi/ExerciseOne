112622ed4275879e9bd01ed84085d9893805ed58
ifrim <christina.ifrim@stud.fh-campus.ac.at>
2022-04-01 21:02:49 +0200

    Finished ExerciseOne

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("NewsApp.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1213, 460);
+        Scene scene = new Scene(fxmlLoader.load(), 1213, 459);
         stage.setTitle("NewsApp from NetSquad!");
         stage.setScene(scene);
         stage.show();
