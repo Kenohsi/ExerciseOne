b189e9423cfac609bb87e3aea524633bab784e05
Keno <kenan.husic@stud.fh-campuswien.ac.at>
2022-04-01 15:15:34 +0200

    Code-Cleanup

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -1,14 +1,10 @@
 package at.ac.fhcampuswien;
 
-import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.PathIterator;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class AppController implements Shape {
+public class AppController {
     private List<Article> articles;
     public String query = "Bitcoin";
 
@@ -64,59 +60,7 @@ public class AppController implements Shape {
         Article four = new Article("The New York Times", "The Bitcoin Case That Puzzled the Shadowy World of Cryptocurrency");
         articles.add(four);
         return articles;
-    }
-
-
-    @Override
-    public Rectangle getBounds() {
-        return null;
-    }
-
-    @Override
-    public Rectangle2D getBounds2D() {
-        return null;
-    }
-
-    @Override
-    public boolean contains(double x, double y) {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Point2D p) {
-        return false;
-    }
-
-    @Override
-    public boolean intersects(double x, double y, double w, double h) {
-        return false;
-    }
-
-    @Override
-    public boolean intersects(Rectangle2D r) {
-        return false;
-    }
-
-    @Override
-    public boolean contains(double x, double y, double w, double h) {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Rectangle2D r) {
-        return false;
-    }
-
-    @Override
-    public PathIterator getPathIterator(AffineTransform at) {
-        return null;
-    }
-
-    @Override
-    public PathIterator getPathIterator(AffineTransform at, double flatness) {
-        return null;
-    }
-}
+    }}
 
 
 
