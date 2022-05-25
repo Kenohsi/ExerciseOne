bcdbd15104b231d309880c7277e33ce4b04ee2df
ahataha123 <84542936+ahataha123@users.noreply.github.com>
2022-05-26 00:28:24 +0200

    Added Source from the last exercise

---
+++ src/main/java/at/ac/fhcampuswien/Source.java
@@ -0,0 +1,22 @@
+package at.ac.fhcampuswien;
+
+public class Source {
+    private String id; //ID of the source (most likely the same as name)
+    private String name; //name of the source
+
+    //Getter and Setter
+    public String getId (){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
